6ea2597a0ca254f5581adc997b2623520bb40597
fffbbb <fffbbb@d93fbb16-7695-4aa9-a2c0-cb93ed53ecbe>
2010-07-28 14:17:17 +0000

    removed old project files

--- net.pkcs11/SessionInformationFlags.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace net.sf.pkcs11net
-{
-	internal class SessionInformationFlags
-	{
-		public const uint CKF_RW_SESSION = 0x00000002;
-		public const uint CKF_SERIAL_SESSION = 0x00000004;
-	}
-}
