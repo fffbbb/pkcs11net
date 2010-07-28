55ebb4ab4de62790922e5d5eac5379822eb5d1e6
fffbbb <fffbbb@d93fbb16-7695-4aa9-a2c0-cb93ed53ecbe>
2010-07-28 14:17:17 +0000

    removed old project files

--- trunk/net.pkcs11/WaitForSlotEventOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System;
-
-namespace net.sf.pkcs11net
-{
-	public enum WaitForSlotEventOptions:uint{
-		doNotBlock=ManifestConstants.CKF_DONT_BLOCK
-	}
-}
--- trunk/net.pkcs11/functions/callbackFunctions.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using net.sf.pkcs11net.generalDataTypes;
-namespace net.sf.pkcs11net.functions
-{
-	
-}
