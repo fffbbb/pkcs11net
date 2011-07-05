a467eecda5cfa00cf17f531fad277345f487fc9b
da_stranger <da_stranger@d93fbb16-7695-4aa9-a2c0-cb93ed53ecbe>
2011-07-05 20:39:34 +0000

    Changed the logic of Mechanism class a bit (in addition to revision 58)

--- pkcs11Wrapper/Pkcs11/Params/Parameters.cs
+++ pkcs11Wrapper/Pkcs11/Params/Parameters.cs
@@ -4,15 +4,31 @@ using Net.Sf.Pkcs11.Wrapper;
 
 namespace Net.Sf.Pkcs11.Params
 {
-	/// <summary>
-	/// Description of Parameters.
-	/// </summary>
-	public class Parameters
-	{
-		
-		public void setParams(CK_MECHANISM ckMechanism){
-			ckMechanism.pParameter=IntPtr.Zero;
-			ckMechanism.ulParameterLen=0;
-		}
-	}
+    /// <summary>
+    /// Description of custom Parameters.
+    /// </summary>
+    public class Parameters
+    {
+        /// <summary>
+        /// Fill CK_MECHANISM according to values of Parameters.
+        /// Default implementation fills everything with zeros.
+        /// </summary>
+        /// <param name="ckMechanism"></param>
+        public virtual void ApplyToMechanism(CK_MECHANISM ckMechanism)
+        {
+            ckMechanism.pParameter = IntPtr.Zero;
+            ckMechanism.ulParameterLen = 0;
+        }
+
+        /// <summary>
+        /// Obtain a parameter class here.
+        /// </summary>
+        /// <param name="ckm"></param>
+        /// <returns></returns>
+        public static Parameters GetParameters(CKM ckm)
+        {
+            Parameters lParameters = new Parameters();
+            return lParameters;
+        }
+    }
 }
